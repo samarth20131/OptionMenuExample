37f5d3d85f5f06cdfd9a0aaa7c8cbc1267fcf788
NiranjanShrimali <niranjankumarshrimali20@gnu.ac.in>
2022-11-09 14:19:02 +0530

    menu created

--- app/src/main/java/com/example/optionmenuexample/MainActivity.kt
+++ app/src/main/java/com/example/optionmenuexample/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.optionmenuexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.action_settings ->{
+                Toast.makeText(applicationContext,"Click on Settiong", Toast.LENGTH_LONG).show()
+                true
+            }
+            R.id.action_share ->{
+                Toast.makeText(applicationContext, "click on share", Toast.LENGTH_LONG).show()
+                return true
+            }
+            R.id.action_exit ->{
+                Toast.makeText(applicationContext, "click on exit", Toast.LENGTH_LONG).show()
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
